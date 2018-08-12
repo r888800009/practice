63033c1162fb9a4827a471efb73e08980d4aa9c3
r888800009 <r888800009@users.noreply.github.com>
2018-08-12 08:27:20 +0800

    Update c412.c

--- zerojudge/c412.c
+++ zerojudge/c412.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define buffer_size 1024
 int main(int argc, char *argv[])
 {
     int n;
     scanf("%d", &n); 
-    getchar();
+    getchar(); 
+    char buffer[buffer_size];
+    int i = 1;
+    i = fread(buffer, 1, buffer_size, stdin);
+    
+    if (i <= 0)
+        return 0;
 
-    for (int i = 0; i < n; i++) {
+    int j = 0; 
+    for (int k = 0; k < n; k++) {
         unsigned long long int o = 0;
         unsigned long long int addW = 0;
         unsigned long long int a = 0;
         unsigned long long int addNew = 0;
+
         char c;
-        while ((c = getchar()) != '\n') {
+        while (i > 0 && (c = buffer[j++]) != '\n') {
             if (c == 'O') {
                 if (addW) {
                     addNew += addW * o;
@@ -29,9 +38,13 @@ int main(int argc, char *argv[])
                 o++;
             } else if(c == 'w' && o > 0)
                 addW++;
+
+            if (j > buffer_size) {
+                i = fread(buffer, 1, buffer_size, stdin);
+                j = 0;
+            }
         }
         printf("%llu\n", a);
-
     }
 
     return 0;
