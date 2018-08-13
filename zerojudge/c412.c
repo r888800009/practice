9dea9a866e609da50489ff9c3a6bdba117f02936
r888800009 <r888800009@users.noreply.github.com>
2018-08-13 20:52:49 +0800

    Update c412.c

--- zerojudge/c412.c
+++ zerojudge/c412.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 #define buffer_size 1024
+#define magic 1000000007 
 int main(int argc, char *argv[])
 {
     int n;
     scanf("%d", &n); 
-    getchar(); 
     char buffer[buffer_size];
     int i = 1;
     i = fread(buffer, 1, buffer_size, stdin);
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     if (i <= 0)
         return 0;
 
-    int j = 0; 
+    int j = 1; 
     for (int k = 0; k < n; k++) {
         unsigned long long int o = 0;
         unsigned long long int addW = 0;
@@ -27,14 +27,15 @@ int main(int argc, char *argv[])
                 if (addW) {
                     addNew += addW * o;
 
-                    if (addNew > 1000000007)
-                        addNew -= 1000000007;
+                    if (addNew > magic)
+                        addNew -= magic;
    
                     addW  = 0;
                 }
+
                 a += addNew;
-                if (a > 1000000007)
-                    a -= 1000000007;
+                if (a > magic)
+                    a -= magic;
                 o++;
             } else if(c == 'w' && o > 0)
                 addW++;
