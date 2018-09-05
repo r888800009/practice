81b647da5df42cebfca5219ef1cae889eb6c3f86
r888800009 <r888800009@users.noreply.github.com>
2018-09-05 09:22:37 +0800

    Update c311.c AC (0ms, 52KB)

--- zerojudge/c311.cpp
+++ zerojudge/c311.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <unistd.h>
-#include <stdint.h>
+#include <cstdint>
 #include <ctype.h>
 
 #define buffer_size 2048
