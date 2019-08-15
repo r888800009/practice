14db153ccaef6b0cc113da9a4d4f394fafb55759
r888800009 <r888800009@users.noreply.github.com>
2019-08-15 22:38:47 +0800

    Update climbing-stairs.c

--- leetcode/climbing-stairs.c
+++ leetcode/climbing-stairs.c
@@ -1,18 +1,17 @@
-int climbStairs(int n){
+int climbStairs(int n) {
   int cur, pre;
-  
-  if (n == 0 || n == 1)
-    return 1;
-  
+
+  if (n == 0 || n == 1) return 1;
+
   pre = 1;
   cur = 2;
-  int tmp; 
-  
+
   for (int i = 3; i <= n; i++) {
+    int tmp;
     tmp = cur + pre;
     pre = cur;
     cur = tmp;
   }
-  
+
   return cur;
 }
