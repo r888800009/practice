c878bb43d7d0064db1caa5ad114e713df21312f7
r888800009 <r888800009@users.noreply.github.com>
2019-09-07 11:29:46 +0800

    add fflush

--- overflow.c
+++ overflow.c
@@ -5,9 +5,11 @@ char shellcode[256];
 int main() {
   char bof[10];
   puts("give me shell code");
+  fflush(stdout);
   gets(shellcode);
 
   puts("Overflow me");
+  fflush(stdout);
   gets(bof);
 
   return 0;
