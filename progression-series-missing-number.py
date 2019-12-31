12fa9e940fc5918e014da51c1eac39617446350c
r888800009 <r888800009@users.noreply.github.com>
2019-12-31 14:44:09 +0800

    Update progression-series-missing-number.py

--- progression-series-missing-number.py
+++ progression-series-missing-number.py
@@ -4,7 +4,7 @@
 
 # 定義上下界限
 # 1 <= sum <= 10 ^ 8
-# 上: 14143 # 10 ^ 8 求出等差數列n與最後項得出
+# 上: 14142 # 10 ^ 8 求出等差數列n與最後項得出
 # 下: 1
 
 def sum(n):
@@ -13,7 +13,7 @@ def sum(n):
 while True:
     sum1 = int(input("give me sum:"))
     print(sum1)
-    up = 14143
+    up = 14142
     down = 1
 
     while down < up and not down + 1 == up:
@@ -24,8 +24,6 @@ while True:
             down = mid
         else:
             up = down = mid + 1
-    if sum(up) - sum1 <= up:
-        print('{} {}'.format(sum(up) - sum1, up))
-    else:
-        print("好像有一些問題")
+
+    print('{} {}'.format(sum(up) - sum1, up))
 
