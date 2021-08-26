8b4b2587250200a674963f793c5becd08ba80d0c
r888800009 <r888800009@gmail.com>
2021-08-26 11:57:10 +0800

    Update c575.py

--- zerojudge/c575.py
+++ zerojudge/c575.py
@@ -10,14 +10,16 @@ while True:
         break
 
     # service point
-    service_point = sorted(input().split())
+    service_point = input().split()
     for i in range(len(service_point)):
         service_point[i] = int(service_point[i]);
-    max_point = service_point[-1]
+    service_point = list(set(service_point))
+    #print(service_point)
+    max_diff = service_point[-1] - service_point[0]
 
     # binary search
     min_r = 1
-    max_r = max_point
+    max_r = max_diff
 
     def count_cell_site(cur_r, service_point):
         count = 1
@@ -33,28 +35,34 @@ while True:
         mid_r = (max_r + min_r) // 2
         mid_cell_count = count_cell_site(mid_r, service_point)
 
-
-        #print(min_r, mid_r, max_r)
-        #print(mid_r, mid_cell_count)
-
-        if min_r == mid_r and mid_cell_count == cell_site:
-            print(mid_r)
-            break
+        """
+        print(min_r, mid_r, max_r)
+        print(f"r:{mid_r}, count:{mid_cell_count}")
+        """
 
         #assert max_r != mid_r
         #assert min_r != mid_r
 
+        # 直徑越小，基地台越多
+        # 直徑越大，基地台越少
+        # 代表
+        # 基地台要減少時，直徑要增加
+        # 基地台要增加時，直徑要減少
+        # 上升下界可以增加直徑 (搜尋直徑範圍 mid+1~max)
+        # 下降上界可以減少直徑 (搜尋直徑範圍 min~mid)
 
-        
+        # 基地台太少需要增加，直徑要減少，下降上界
         if mid_cell_count < cell_site:
-            max_r = max(mid_r - 1, mid_r)
+            #max_r = max(mid_r - 1, min_r)
+            # why max_r 上界不需要 - 1? (可能跟整數除法有關)
+            max_r = mid_r
+        # 基地台太多需要減少，直徑要增加，上升下界
         elif mid_cell_count > cell_site:
             min_r = min(mid_r + 1, max_r)
+        # 基地台剛好，但是可能會有相同基地台數量，因此下降上界減少直徑
         elif min_r != max_r and mid_cell_count == cell_site:
             max_r = mid_r
 
-        if min_r == max_r:
-            print(mid_r)
-            #print(mid_r, '1', mid_cell_count)
+    print(min_r)
 
 
