bb235761336f8b02dc57cae582800196c3ca78c5
r888800009 <r888800009@gmail.com>
2021-08-26 09:32:54 +0800

    Update c575.py

--- zerojudge/c575.py
+++ zerojudge/c575.py
@@ -1,55 +1,60 @@
 #!/usr/bin/env python
 # NA QQ
-# N K
-line = input().split()
-cell_site = int(line[1])
-
-# service point
-service_point = sorted(input().split())
-for i in range(len(service_point)):
-    service_point[i] = int(service_point[i]);
-max_point = service_point[-1]
-
-# binary search
-min_r = 1
-max_r = max_point
-
-def count_cell_site(cur_r, service_point):
-    count = 1
-    cur = service_point[0]
-    for point in service_point:
-        if cur + cur_r < point:
-            count += 1
-            cur = point
-
-    return count
-
-while min_r != max_r:
-    mid_r = (max_r + min_r) // 2
-    mid_cell_count = count_cell_site(mid_r, service_point)
-
-
-    #print(min_r, mid_r, max_r)
-    #print(mid_r, mid_cell_count)
-
-    if min_r == mid_r and mid_cell_count == cell_site:
-        print(mid_r)
+while True:
+    # N K
+    cell_site = -1
+    try:
+        line = input().split()
+        cell_site = int(line[1])
+    except EOFError:
         break
 
-    #assert max_r != mid_r
-    #assert min_r != mid_r
+    # service point
+    service_point = sorted(input().split())
+    for i in range(len(service_point)):
+        service_point[i] = int(service_point[i]);
+    max_point = service_point[-1]
+
+    # binary search
+    min_r = 1
+    max_r = max_point
+
+    def count_cell_site(cur_r, service_point):
+        count = 1
+        cur = service_point[0]
+        for point in service_point:
+            if cur + cur_r < point:
+                count += 1
+                cur = point
+
+        return count
+
+    while min_r != max_r:
+        mid_r = (max_r + min_r) // 2
+        mid_cell_count = count_cell_site(mid_r, service_point)
 
 
-    
-    if mid_cell_count < cell_site:
-        max_r = max(mid_r - 1, mid_r)
-    elif mid_cell_count > cell_site:
-        min_r = min(mid_r + 1, max_r)
-    elif min_r != max_r and mid_cell_count == cell_site:
-        max_r = mid_r
+        #print(min_r, mid_r, max_r)
+        #print(mid_r, mid_cell_count)
 
-    if min_r == max_r:
-        print(mid_r)
-        #print(mid_r, '1', mid_cell_count)
+        if min_r == mid_r and mid_cell_count == cell_site:
+            print(mid_r)
+            break
+
+        #assert max_r != mid_r
+        #assert min_r != mid_r
+
+
+        
+        if mid_cell_count < cell_site:
+            max_r = max(mid_r - 1, mid_r)
+        elif mid_cell_count > cell_site:
+            min_r = min(mid_r + 1, max_r)
+        elif min_r != max_r and mid_cell_count == cell_site:
+            max_r = mid_r
+
+        if min_r == max_r:
+            print(mid_r)
+            #print(mid_r, '1', mid_cell_count)
 
 
