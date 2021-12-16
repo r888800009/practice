191bfb5c1d111d8a57f3dfad126e724f5e263a0d
r888800009 <r888800009@users.noreply.github.com>
2021-12-17 07:47:18 +0800

    Add small-bin-2.c

---
+++ pwn/allocator/small-bin-2.c
@@ -0,0 +1,29 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int *ptr1[20];
+  int *ptr2[20];
+
+  for (size_t i = 0; i < 13; i++) {
+    int *split;
+    ptr1[i] = malloc(0x100);
+    split = malloc(0x10);
+    ptr2[i] = malloc(0x110);
+    split = malloc(0x10);
+  }
+
+  for (size_t i = 0; i < 13; i++) {
+    free(ptr1[i]);
+    raise(SIGINT);
+    free(ptr2[i]);
+    raise(SIGINT);
+  }
+
+  malloc(0x10);
+
+  raise(SIGINT);
+
+  return 0;
+}
