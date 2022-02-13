7581f17ab74ecd6a98ae5a65bf240af17651e5ba
r888800009 <r888800009@users.noreply.github.com>
2022-02-13 09:56:49 +0800

    trigging malloc_consolidate

--- pwn/allocator/fast-bin.c
+++ pwn/allocator/fast-bin.c
@@ -12,12 +12,19 @@ int main(int argc, char *argv[]) {
     ptr2[i] = malloc(0x50);
   }
 
-  for (size_t i = 0; i < 15; i++) {
+  for (size_t i = 0; i < 13; i++) {
     free(ptr2[i]);
     raise(SIGINT);
     free(ptr1[i]);
     raise(SIGINT);
   }
+
+  malloc(0x400);
+  raise(SIGINT);
+  free(ptr2[13]);
+  raise(SIGINT);
+  free(ptr1[13]);
+  raise(SIGINT);
 
   raise(SIGINT);
 
