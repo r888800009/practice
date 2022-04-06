5b44e3668f4a1e0124cfd4864169ef313ed3097b
r888800009 <r888800009@users.noreply.github.com>
2022-04-06 23:25:26 +0800

    Add libfuzz.c

---
+++ pwn/fuzz/libfuzz.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(const uint8_t *Data, size_t Size) {
+  if (Data[0] == 'a') {
+    // trigger a crash
+    puts("crash");
+    int *p = 0;
+    *p = 0;
+  }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  foo(Data, Size);
+  return 0;
+}
