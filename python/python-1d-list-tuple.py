1a7d02fd39d6de9e221bc342648b2d45d4ef6455
Lin, Yong Xiang <r888800009@gmail.com>
2023-04-06 01:26:00 +0800

    Update and add 1d list tuple evaluation

--- python/python-1d-list-tuple.py
+++ python/python-1d-list-tuple.py
@@ -6,43 +6,36 @@ import time
 
 print("""this file is evaluate python set vs fixed size list hashtable""")
 
-access_time = 10000
+access_time = 10000 **2
 
 for size in range(1000, 1000*10, 1000):
     print("size", size)
 
     print("init")
-    list_2d = []
-    tuple_2d = tuple()
-    for i in range(size):
-        list_2d.append([j for j in range(size)])
-        tuple_2d += (tuple([j for j in range(size)]),)
+    list_1d = [j for j in range(size)]
+    tuple_1d = tuple([j for j in range(size)])
 
     start = time.time()
     for i in range(size):
-        for j in range(size):
-            a = tuple_2d[i][j] == 3
+        a = tuple_1d[i] == 3
     end = time.time()
     print("random access tuple", end - start)
 
     start = time.time()
     for i in range(size):
-        for j in range(size):
-            a = list_2d[i][j] == 3
+        a = list_1d[i] == 3
     end = time.time()
     print("random access list", end - start)
 
     start = time.time()
-    for internal_list in tuple_2d:
-        for item in internal_list:
-            a = item == 3
+    for item in tuple_1d:
+        a = item == 3
     end = time.time()
-    print("traversing `in`", end - start)
+    print("traversing tuple `in`", end - start)
 
     start = time.time()
-    for internal_list in list_2d:
-        for item in internal_list:
-            a = item == 3
+    for item in list_1d:
+        a = item == 3
     end = time.time()
     print("traversing list `in`", end - start)
 
